e6d9f18264c6e86c4674bb17da920a27be3b5bfb
Ben Eisner <ben.a.eisner@gmail.com>
2011-12-28 00:20:24 -0500

    Finalized git ignore

--- HiddenCompilation/HCShell/HCShell/Shell.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-	system("\"\"C:\\Users\\Ben\\Autograder-0.1\\HiddenCompilation\\HiddenCompile\\Release\\HiddenCompile.exe\" C:\\Users\\Ben\\Autograder-0.1\\AutoGraderVS\\AutoGraderVS\\QuizDocs\\Quiz1\\Stud1.cpp\" C:\\Users\\Ben\\Autograder-0.1\\AutoGraderVS\\AutoGraderVS\\QuizDocs\\Quiz1\\Stud1.cpp\"");
-}
