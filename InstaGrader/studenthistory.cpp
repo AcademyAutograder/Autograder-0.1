459fe1cc09af12aed696349bf703a0b65f1c416a
Sanmeshkumar Udhayakumar <sanmesh1@gmail.com>
2012-01-22 22:09:18 -0500

    tableviews

--- InstaGrader/studenthistory.cpp
+++ InstaGrader/studenthistory.cpp
@@ -6,9 +6,9 @@ studenthistory::studenthistory(QWidget *parent) :
     ui(new Ui::studenthistory)
 {
     ui->setupUi(this);
-    listmodel = new QStringListModel (this);
-    listmodel->setStringList(list);
-    ui->listView->setModel(listmodel);
+    //listmodel = new QStringListModel (this);
+    //listmodel->setStringList(list);
+    //ui->listView->setModel(listmodel);
 }
 
 studenthistory::~studenthistory()
@@ -16,9 +16,7 @@ studenthistory::~studenthistory()
     delete ui;
 }
 
-
-
-void studenthistory::on_listView_activated(const QModelIndex &index)
+void studenthistory::on_tableView_activated(const QModelIndex &index)
 {
     tablecommonstudent = new commontable;
     tablecommonstudent -> show();
